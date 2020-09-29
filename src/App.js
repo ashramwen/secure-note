269c0b3d87cdde2eae30cc9c1b3dd104f3cc9eac
Augustine <ashramwen@gmail.com>
2020-09-29 14:11:54 +0800

    feat: 🎸 Add styled-normalize

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Normalize } from 'styled-normalize';
 import GlobalStyle from 'styles/global-styles';
 import SecureNotes from 'components/SecureNotes';
 
 function App() {
   return (
     <>
+      <Normalize />
       <GlobalStyle />
       <SecureNotes />
     </>
