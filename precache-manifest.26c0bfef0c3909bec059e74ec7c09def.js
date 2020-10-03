7bc30aa899d086d21d6093a103820acdfec05df6
Augustine <ashramwen@gmail.com>
2020-10-03 17:58:16 +0800

    Updates

---
+++ precache-manifest.26c0bfef0c3909bec059e74ec7c09def.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb5c5dca19550d204ea3ff6456ffe7cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da7fa92a2ee8bf2211de",
+    "url": "/static/js/2.e9401833.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.e9401833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b9fe4165030dfa0492d",
+    "url": "/static/js/main.d0ec6537.chunk.js"
+  },
+  {
+    "revision": "0c226c8e8a8ad990093b",
+    "url": "/static/js/runtime-main.222659de.js"
+  },
+  {
+    "revision": "37465d671030b24b9d439380f0a95d87",
+    "url": "/static/media/cancel.37465d67.svg"
+  },
+  {
+    "revision": "ceefb4de1ace1ba9a97abf521d753442",
+    "url": "/static/media/close.ceefb4de.svg"
+  },
+  {
+    "revision": "370111b1a785367138e3e11f0df5f5b8",
+    "url": "/static/media/delete.370111b1.svg"
+  },
+  {
+    "revision": "232686a07cc7aa55ea2d6f84eb381091",
+    "url": "/static/media/edit.232686a0.svg"
+  },
+  {
+    "revision": "61d72d1a588c4d6e8f2468631143a1fc",
+    "url": "/static/media/plus.61d72d1a.svg"
+  },
+  {
+    "revision": "51ed5ea8f9b561b61ecdb634361a3c41",
+    "url": "/static/media/save.51ed5ea8.svg"
+  },
+  {
+    "revision": "e55cb6b8b217f9211c8905e1345eecaf",
+    "url": "/static/media/saving.e55cb6b8.svg"
+  },
+  {
+    "revision": "85759e84af126ae3978d701eaa82f1cc",
+    "url": "/static/media/spinner.85759e84.svg"
+  }
+]);
